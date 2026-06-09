41bc3d09c4b0ae1225f33b5d86df41e38eb08c0d
Kushagra-tripathi-ui <kushagratripathi3003@gmail.com>
2026-06-09 22:42:26 +0530

    Add RegisterRequest DTO

---
+++ src/main/java/com/payment/dto/RegisterRequest.java
@@ -0,0 +1,37 @@
+package com.payment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+    
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    // At least 8 chars, 1 uppercase, 1 lowercase, 1 digit, 1 special char
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        message = "Password must contain uppercase, lowercase, digit, and special character"
+    )
+    private String password;
+    
+    @NotBlank(message = "First name is required")
+    private String firstName;
+    
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+    
+    private String phone;
+}
